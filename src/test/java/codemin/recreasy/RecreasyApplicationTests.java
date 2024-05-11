5014fac555db6df9636944b255faff548fcae7f9
Rohdamin <rohdamin3@gmail.com>
2024-05-11 21:39:24 +0900

    init

---
+++ src/test/java/codemin/recreasy/RecreasyApplicationTests.java
@@ -0,0 +1,13 @@
+package codemin.recreasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecreasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
